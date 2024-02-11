ab3f5d4da962cf2d4ce95ae7a3e2bce8ee02e8e5
krishnaprasadnr702119 <krishnaprasadnr702119>
2024-02-11 23:05:51 +0530

    filter

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task/data/database_helper.dart';
 import 'package:task/forgetpassword/screen/bloc/forgetpassword_bloc.dart';
-import 'package:task/login/screen/login.dart'; // Import your LoginPage here
+import 'package:task/login/screen/login.dart';
 import 'package:task/login/bloc/login_bloc.dart';
 import 'package:task/signup/bloc/registration_bloc.dart';
 import 'package:task/screens/splashscreen.dart';
